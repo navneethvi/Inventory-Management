b8b518fd2c2c266fd0b9342c319390c2fd7039a2
Navaneeth V <navaneethvinod18@gmail.com>
2025-01-12 16:57:18 +0530

    cors 3

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,12 +7,14 @@ const port = 3000;
 
 const corsOptions = {
   origin: [
-    'https://inventory-frontend-wheat.vercel.app',
-    'https://inventory-ec0e4zbvc-navneethvis-projects.vercel.app',
-  ],  methods: ['GET'],
+    'https://inventory-frontend-wheat.vercel.app',  // Frontend URL
+    'https://inventory-frontend-pb73ndv9h-navneethvis-projects.vercel.app'          // Add any other URLs that should be allowed
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 };
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
